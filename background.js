4a7d722697619997bf79875ce18e531cdacc02ec
lEric-Wl <eric.wang887@gmail.com>
2025-08-16 13:45:19 -0400

    Using a background script to detect link changes using browser.webNavigation

---
+++ background.js
@@ -0,0 +1,6 @@
+function listener(details){
+  console.log("updated", details.url)
+  browser.tabs.sendMessage(details.tabId, {action: "redirect"})
+}
+
+browser.webNavigation.onHistoryStateUpdated.addListener(listener,null);
--- script.js
+++ script.js
@@ -2,15 +2,24 @@ function redirect(link = document.URL) {
     if (link.includes("shorts")) {
         console.log("shorts detected");
         let videoId = link.split("/shorts/")[1].split("?")[0];
-        history.pushState(null,"","https://www.youtube.com/watch?v=" + videoId);
-        window.location.reload()
+        window.location.replace("https://www.youtube.com/watch?v=" + videoId);
         console.log("redirected");
     }
 }
 
-document.addEventListener("click", () => { //This is the most reliable way check for shorts content I found so far
+/*
+document.addEventListener("click", (e) => { //This is the most reliable way check for shorts content I found so far
     console.log("click event triggered");
+    target = e.target.closest("a");
+    console.log("target",target);
     redirect();
 });
+*/
 
-redirect();
+browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.action === "redirect") {
+        redirect();
+        sendResponse({status: "done"});
+        return true; // Important: lets the browser know you'll respond asynchronously
+    }
+});
