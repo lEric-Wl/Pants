9cea38620f676f16d2eca45b261fee8bf748857f
lEric-Wl <eric.wang887@gmail.com>
2025-08-16 18:26:03 -0400

    removed unused block of code, and added 250ms to to make redirecting smoother

--- background.js
+++ background.js
@@ -1,6 +1,8 @@
 function listener(details){
   console.log("updated", details.url);
-  browser.tabs.sendMessage(details.tabId, {action: "redirect"});
+  setTimeout(() =>{
+    browser.tabs.sendMessage(details.tabId, {action: "redirect"});
+  },250)
 }
 
 browser.webNavigation.onHistoryStateUpdated.addListener(listener,null);
--- script.js
+++ script.js
@@ -7,19 +7,10 @@ function redirect(link = document.URL) {
     }
 }
 
-/*
-document.addEventListener("click", (e) => { //This is the most reliable way check for shorts content I found so far
-    console.log("click event triggered");
-    target = e.target.closest("a");
-    console.log("target",target);
-    redirect();
-});
-*/
-
 browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.action === "redirect") {
         redirect();
         sendResponse({status: "done"});
-        return true; // Important: lets the browser know you'll respond asynchronously
+        return true; 
     }
 });
