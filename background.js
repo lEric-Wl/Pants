ec12df802e3b7e1356fbc5c29d04cc34eff0e582
lEric-Wl <eric.wang887@gmail.com>
2025-08-17 11:43:52 -0400

    Added comment

--- background.js
+++ background.js
@@ -2,7 +2,7 @@ function listener(details){
   console.log("updated", details.url);
   setTimeout(() =>{
     browser.tabs.sendMessage(details.tabId, {action: "redirect"}); //Signals to main script to run redirect
-  },250)
+  },250) //Added delay to make it smoother, else trying to load in the main player immediately will cause a no connection error
 }
 
 //Param: function, website filter
